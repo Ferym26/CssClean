0f9cb03f401a9963049de9c7300e0c68d658d01a
ferym26 <ferym26@gmail.com>
2016-10-14 11:13:19 +0300

    bug

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
-var gulp     = require('gulp'),
-		uncss    = require('gulp-uncss-sp'),
-		rename   = require('gulp-rename'),
-		glob     = require('glob');
+var gulp           = require('gulp'),
+
+		uncss          = require('gulp-uncss'),
+		uncssp          = require('gulp-uncss-sp'),
+
+		autoprefixer   = require('gulp-autoprefixer'),
+		rename         = require('gulp-rename'),
+		glob           = require('glob');
 
  
 gulp.task('cssclean', function() {
-    gulp.src('css/*.css')
-        .pipe(uncss({
-            html: glob.sync('html/**/*.html')
-        }))
-        .pipe(rename({suffix: '.clean', prefix : ''}))
-        .pipe(gulp.dest('./out'));
+	gulp.src('./css/*.css')
+		.pipe(uncssp({
+			//html: glob.sync('html/**/*.html')
+			html: ['./html/index.html']
+		}))
+		.pipe(autoprefixer({
+			browsers: ['last 5 versions'],
+			cascade: false
+		}))
+		.pipe(rename({
+			suffix: '.clean',
+			prefix : ''
+		}))
+		.pipe(gulp.dest('./out'));
 });
